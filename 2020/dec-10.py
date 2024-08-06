860d630ceaf829d7f6953ff82c0cb0d022eb5214
Vikram Parikh <vmparikh91@gmail.com>
2024-08-06 12:53:44 +0100

    feat: add day 10 solution

---
+++ 2020/dec-10.py
@@ -0,0 +1,37 @@
+from functools import lru_cache
+
+
+def arrangements(nums, i):
+	@lru_cache()
+	def real_fn(i):
+		if i == len(nums) - 1:
+			return 1
+
+		tot = 0
+		for j in range(i + 1, min(i + 4, len(nums))):
+			if nums[j] - nums[i] <= 3:
+				tot += real_fn(j)
+
+		return tot
+
+	return real_fn(i)
+
+
+with open("input.txt", "r") as fin:
+	nums = sorted(map(int, fin))
+	nums = [0] + nums + [max(nums) + 3]
+
+dist1 = dist3 = 0
+for cur, nxt in zip(nums, nums[1:]):
+    delta = nxt - cur
+
+    if delta == 1:
+        dist1 += 1
+    elif delta == 3:
+        dist3 += 1
+
+ans = dist1 * dist3
+print('Part 1:', ans)
+
+total = arrangements(nums, 0)
+print('Part 2:', total)
