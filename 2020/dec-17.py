6c5a27c97594a3b2f91fe3b9ee502c28b6e681fa
Vikram Parikh <vmparikh91@gmail.com>
2024-08-07 12:08:32 +0100

    feat: add day 17 solution

---
+++ 2020/dec-17.py
@@ -0,0 +1,43 @@
+from itertools import product
+
+
+def neighbors(coords):
+	ranges = ((c - 1, c, c + 1) for c in coords)
+	yield from product(*ranges)
+
+def all_neighbors(cube):
+	return set(n for p in cube for n in neighbors(p))
+
+def alive_neighbors(cube, coords):
+	return sum(p in cube for p in neighbors(coords)) - (coords in cube)
+
+def evolve(cube):
+	new = set()
+
+	for p in all_neighbors(cube):
+		alive = alive_neighbors(cube, p)
+
+		if alive == 3 or (alive == 2 and p in cube):
+			new.add(p)
+
+	return new
+
+
+with open("input.txt", "r") as fin:
+	grid = tuple(map(str.rstrip, fin))
+	h, w = len(grid), len(grid[0])
+	cube = set((x, y, 0) for x, y in product(range(h), range(w)) if grid[x][y] == '#')
+
+for _ in range(6):
+	cube = evolve(cube)
+
+total_alive = len(cube)
+print('Part 1:', total_alive)
+
+hypercube = set((x, y, 0, 0) for x, y in product(range(h), range(w)) if grid[x][y] == '#')
+
+for _ in range(6):
+	hypercube = evolve(hypercube)
+
+total_alive = len(hypercube)
+print('Part 2:', total_alive)
