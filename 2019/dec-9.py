fc78e3073b3bd2e08038c491021fb347c9eb2284
Vikram Parikh <vmparikh91@gmail.com>
2024-08-12 11:25:14 +0100

    feat: add day 9 solution

---
+++ 2019/dec-9.py
@@ -0,0 +1,64 @@
+from fractions import gcd
+from math import atan2, pi as PI
+
+
+def ray(ax, ay, bx, by):
+	dx, dy = bx - ax, by - ay
+	div = abs(gcd(dx, dy))
+	return dx//div, dy//div
+
+def manhattan(ax, ay, bx, by):
+	return abs(bx - ax) + abs(by - ay)
+
+def angle(ax, ay, bx, by):
+	rad = atan2(by-ay, bx-ax) + PI
+	rad = rad % (2*PI) - PI/2
+	return rad if rad >= 0 else 2*PI + rad
+
+
+with open("input.txt", "r") as fin:
+	grid = [l.rstrip() for l in fin]
+
+asteroids = set()
+for y, row in enumerate(grid):
+	for x, cell in enumerate(row):
+		if cell == '#':
+			asteroids.add((x, y))
+
+station = None
+max_in_sight = 0
+
+for src in asteroids:
+	lines_of_sight = set()
+
+	for a in asteroids:
+		if a == src:
+			continue
+
+		lines_of_sight.add(ray(*src, *a))
+
+	in_sight = len(lines_of_sight)
+	if in_sight > max_in_sight:
+		max_in_sight = in_sight
+		station = src
+
+print('Part 1:', max_in_sight)
+
+closest = {}
+target = 200
+
+for a in asteroids:
+	if a == station:
+		continue
+
+	r = ray(*station, *a)
+	m = manhattan(*station, *a)
+
+	if r not in closest or m < closest[r][1]:
+		closest[r] = (a, m)
+
+ordered = sorted(closest.values(), key=lambda am: angle(*station, *am[0]))
+chosen_x, chosen_y = ordered[target - 1][0]
+ans = 100 * chosen_x + chosen_y
+
+print('Part 2:', ans)
