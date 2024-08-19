67d9a62ea534b17998fcdd1f85ad917586f8b5f8
Vikram Parikh <vmparikh91@gmail.com>
2024-08-19 14:54:24 +0100

    feat: add day 8 solution

---
+++ 2018/dec-8.py
@@ -0,0 +1,43 @@
+from collections import namedtuple
+
+
+Node = namedtuple('Node', ['length', 'children', 'metadata'])
+
+def build_tree(flat):
+	nchilds  = flat[0]
+	nmeta    = flat[1]
+	length   = 2
+	children = []
+
+	for _ in range(nchilds):
+		child = build_tree(flat[length:])
+		children.append(child)
+		length += child.length
+
+	return Node(length + nmeta, children, flat[length:length + nmeta])
+
+def sum_meta(node):
+	return sum(node.metadata) + sum(sum_meta(c) for c in node.children)
+
+def node_value(node):
+	if not node.children:
+		return sum(node.metadata)
+
+	value = 0
+	for i in node.metadata:
+		if 1 <= i <= len(node.children):
+			value += node_value(node.children[i - 1])
+
+	return value
+
+
+with open("input.txt", "r") as fin:
+	nums = list(map(int, fin.read().split()))
+
+root = build_tree(nums)
+meta_sum = sum_meta(root)
+
+print('Part 1:', meta_sum)
+
+value = node_value(root)
+print('Part 2:', value)
