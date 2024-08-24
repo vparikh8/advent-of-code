ca1bdd7bd27f65fe4408b000be17f6676386e145
Vikram Parikh <vmparikh91@gmail.com>
2024-08-24 11:50:33 +0100

    feat: add day 13 solution

--- 2018/dec-13.py
+++ 2018/dec-13.py
@@ -1,88 +1,46 @@
-import sys
-from copy import deepcopy
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
 
-debug = False
+tracks = {x + y*1j: t for y, l in enumerate(lines) for x, t in enumerate(l)}
+dirs = {'>': 1, '<': -1, '^': -1j, 'v': 1j}
 
-m = []
-with open("input.txt", "r") as fin:
-    m.append(list(fin.readlines()))
+def make_carts():
+    return [[pos, dirs[d], 0] for pos, d in tracks.items() if d in dirs]
 
-minecarts = []
-d = {'<': (0, -1), '>': (0, 1), '^': (-1, 0), 'v': (1, 0)}
-t = {
-    0: (lambda r, c: (c, r) if r else (-c, -r)),
-    2: (lambda r, c: (c, r) if c else (-c, -r)),
-    1: (lambda r, c: (r, c))
-}
+def move_cart(cart):
+    track = tracks[cart[0]]
+    if track == '+':
+        if cart[2] == 0:
+            cart[1] *= -1j
+        elif cart[2] == 2:
+            cart[1] *= 1j
+        cart[2] = (cart[2] + 1) % 3
+    elif track in ('/', '\\'):
+        cart[1] *= 1j
+        if (cart[1].imag and track == '/') or (cart[1].real and track == '\\'):
+            cart[1] *= -1
+    cart[0] += cart[1]
 
-p = {'<': '-', '>': '-', '^': '|', 'v': '|'}
-for r in range(len(m)):
-    for c in range(len(m[0])):
-        if m[r][c] in '<>^v':
-            dr, dc = d[m[r][c]]
-            m[r][c] = p[m[r][c]]
-            minecarts.append([r, c, dr, dc, 0])
-minecarts2 = deepcopy(minecarts)
+# Part one
+def part_one():
+    carts = make_carts()
+    while True:
+        for cart in carts:
+            move_cart(cart)
+            if any(c for c in carts if c != cart and c[0] == cart[0]):
+                return cart[0]
+print(part_one())
 
-def draw():
-    m2 = deepcopy(m)
-    for r, c, *_ in minecarts:
-        m2[r][c] = '\033[91m#\033[0m'
-    ret = []
-    for r in m2:
-        ret.append(''.join(r))
-    print('\n'.join(ret))
-
-crash = False
-cx, cy = None, None
-while not crash:
-    seen, crash = set(), False
-    minecarts.sort(key=lambda x: x[:2])
-    for minecart in minecarts:
-        minecart[0] += minecart[2]
-        minecart[1] += minecart[3]
-        r, c, dr, dc, mt = minecart
-        if m[r][c] == '\\':
-            minecart[2], minecart[3] = dc, dr
-        elif m[r][c] == '/':
-            minecart[2], minecart[3] = -dc, -dr
-        elif m[r][c] == '+':
-            minecart[2], minecart[3] = t[mt](dr, dc)
-            minecart[4] = (mt + 1) % 3
-        if tuple(minecart[:2]) in seen:
-            crash, cx, cy = True, minecart[1], minecart[0]
-        seen.add(tuple(minecart[:2]))
-    if debug:
-        for mc in sorted(minecarts):
-            print(mc)
-        draw()
-print('Part 1:', f'{cx},{cy}')
-
-minecarts = minecarts2
-while len(minecarts) != 1:
-    new_minecarts = []
-    crash = set()
-    minecarts.sort(key=lambda x: x[:2])
-    for i, minecart in enumerate(minecarts):
-        minecart[0] += minecart[2]
-        minecart[1] += minecart[3]
-        r, c, dr, dc, mt = minecart
-        if m[r][c] == '\\':
-            minecart[2], minecart[3] = dc, dr
-        elif m[r][c] == '/':
-            minecart[2], minecart[3] = -dc, -dr
-        elif m[r][c] == '+':
-            minecart[2], minecart[3] = t[mt](dr, dc)
-            minecart[4] = (mt + 1) % 3
-        for i2, minecart2 in enumerate(minecarts):
-            if i != i2 and minecart[:2] == minecart2[:2]:
-                crash |= {i, i2}
-    for i in range(len(minecarts)):
-        if i not in crash:
-            new_minecarts.append(minecarts[i])
-    minecarts = new_minecarts
-    if debug:
-        for mc in sorted(minecarts):
-            print(mc)
-        draw()
-print('Part 2:', f'{minecarts[0][1]},{minecarts[0][0]}')
+# Part two
+def part_two():
+    carts = make_carts()
+    while True:
+        if len(carts) == 1:
+            return carts[0][0]
+        carts.sort(key=lambda cart: cart[0].imag)
+        for cart in carts:
+            move_cart(cart)
+            crashed = [c for c in carts if c != cart and c[0] == cart[0]]
+            if crashed:
+                carts = [c for c in carts if c != cart and c != crashed[0]]
+print(part_two())
